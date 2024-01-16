bd820d52e1dcab6e0855191ff270cb8ef4b335a7
nkc930604@gmail.com <nkc930604@gmail.com>
2024-01-16 12:31:11 +0900

    Feat: "add hashtag search"
    해시태그 검색 기능 추가

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+import PostBox from "components/posts/PostBox";
+import AuthContext from "context/AuthContext";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "firebaseApp";
+import { PostProps } from "pages/home";
+import React, { useContext, useEffect, useState } from "react";
 
 const SearchPage = () => {
-  return <h1>검색 페이지</h1>;
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [tagQuery, setTagQuery] = useState<string>("");
+  const { user } = useContext(AuthContext);
+
+  console.log(tagQuery);
+
+  const onChange = (e: any) => {
+    setTagQuery(e.target.value.trim());
+  };
+
+  useEffect(() => {
+    if (user) {
+      let postRef = collection(db, "posts");
+      let postsQuery = query(
+        postRef,
+        where("hashTag", "array-contains-any", [tagQuery]),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(postsQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [tagQuery, user]);
+
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">
+          <div className="home__title-text">Search</div>
+        </div>
+        <div className="home__search-div">
+          <input
+            type="text"
+            className="home__search"
+            placeholder="해시태그 검색"
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <div className="post">
+        {posts.length > 0 ? (
+          posts.map((post) => <PostBox post={post} key={post.id} />)
+        ) : (
+          <div className="post__no-posts">
+            <div className="post__text">게시글이 없습니다.</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
